3d7260355c24e40b139018053a5d5e88c725e4de
tmingora <tmingora@enssat.fr>
2020-03-11 18:14:30 +0100

    Début essais successifs

--- src/EssaisSuccessifs.java
+++ src/EssaisSuccessifs.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class EssaisSuccessifs {
+
+
+}
+
+class Polygone{
+    public int nbSommets;
+    public ArrayList<int[]> cordes;
+
+    public boolean validecorde(int i, int j){
+        ArrayList
+    }
 }
