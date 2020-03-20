e80f37c4526283ae804f379070c20d45b110af1d
tmingora <tmingora@enssat.fr>
2020-03-20 11:14:54 +0100

    PseudoCode essaisSuccessifs

--- src/essaisSuccessifs/EssaisSuccessifs.java
+++ src/essaisSuccessifs/EssaisSuccessifs.java
@@ -7,7 +7,20 @@ import static java.lang.Integer.min;
 
 public class EssaisSuccessifs {
 
+/*Procédure essaisSuccessifs
+param : un Polygone
+    Si on ne peux plus faire de corde : // à remanier pour inclure dans l'autre boucle
+        enregistrer la config des cordes du Polygone
+        Fin de procédure
+    FSi
+    Pour tous les sommets a :
+        Pour tous les sommets b pour lesquels on peut faire une corde entre a et b :
+            Faire la corde
+            lancer essaisSuccessifs sur le nouveau Polygone qui a la corde en plus
+        FPour
+    FPour
 
+ */
 
     public static void main (String [] args){
 
